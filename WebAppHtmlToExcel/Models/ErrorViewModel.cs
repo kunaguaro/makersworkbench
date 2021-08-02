98a12ef8050e15aa4f4c69a15244e49818f0cafb
kunaguaro <omar.jesus.rodriguez@gmail.com>
2021-08-02 15:07:39 -0500

    Agregar archivos de proyecto.

---
+++ WebAppHtmlToExcel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppHtmlToExcel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
